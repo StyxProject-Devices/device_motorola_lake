93ab0d44457265f80cae4ee9ccfa74f807cfa40d
Francisco Sánchez M <franthekillaz@gmail.com>
2021-07-09 07:48:06 -0400

    lake: Use Redfin July 2021 fp
    Signed-off-by: Fraaxius <franthekillaz@gmail.com>

--- init/init_lake.cpp
+++ init/init_lake.cpp
@@ -83,23 +83,23 @@ void vendor_load_device_properties()
     bootsku = GetProperty("ro.boot.hardware.sku", "");
     if (bootsku == "XT1965-T") {
         /* T-Mobile REVVLRY+ */
-        property_override_device("ro.build.description", "redfin-user 11 RQ2A.210505.003 7255357 release-keys");
+        property_override_device("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
         property_override_device("persist.vendor.radio.customer_mbns", "tmo_usa_ims_default.mbn");
         property_override_device("persist.vendor.radio.data_con_rprt", "1");
         property_override_device("persist.vendor.ims.playout_delay", "10");
         property_override_device("persist.vendor.ims.cam_sensor_delay", "20");
         property_override_device("persist.vendor.ims.display_delay", "40");
         for (const auto &source : ro_props_default_source_order) {
-            set_ro_build_prop(source, "fingerprint", "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys");
+            set_ro_build_prop(source, "fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
             set_ro_product_prop(source, "device", "lake_n");
             set_ro_product_prop(source, "model", "REVVLRY+");
             set_ro_product_prop(source, "name", "lake_revvl_n");
         }
     } else {
         /* moto g(7) plus (Unlocked) */
-        property_override_device("ro.build.description", "redfin-user 11 RQ2A.210505.003 7255357 release-keys");
+        property_override_device("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
         for (const auto &source : ro_props_default_source_order) {
-            set_ro_build_prop(source, "fingerprint", "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys");
+            set_ro_build_prop(source, "fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
             set_ro_product_prop(source, "device", "lake");
             set_ro_product_prop(source, "model", "moto g(7) plus");
         }
--- styx_lake.mk
+++ styx_lake.mk
@@ -70,9 +70,9 @@ PRODUCT_RELEASE_NAME := lake
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lake \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
 
 # StyxOS Maintainer
 STYX_BUILD_VARIANT := OFFICIAL
